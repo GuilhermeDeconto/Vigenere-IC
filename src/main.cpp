f577764f08f03f1ef95e9b8b38d054b85690b831
Guilherme Dall'Agnol Deconto <deconto100@gmail.com>
2021-08-25 20:28:11 -0300

    Creating main header file and using chrono to ckeck application elapsed time

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "io.h"
-#include <iostream>
+#include "main.h"
 
 int main() {
+    //Starting chrono clock
+    auto start = chrono::steady_clock::now();
+
     string input = io::getStringFromFile();
     std::cout << "Hello, World!" << std::endl;
+
+
+    //Ending chrono clock
+    auto end = chrono::steady_clock::now();
+    //Checking elapsed time
+    cout << "Elapsed time in milliseconds: " << chrono::duration_cast<chrono::milliseconds>(end - start).count() << " ms";
+
     return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,17 @@
+//
+// Created by Guilherme Deconto on 8/25/21.
+//
+
+#ifndef VIGENERE_IC_MAIN_H
+#define VIGENERE_IC_MAIN_H
+
+#include <iostream>
+#include <chrono>
+
+class main {
+
+
+
+};
+
+#endif //VIGENERE_IC_MAIN_H
