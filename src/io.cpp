92153f00e028b3c20719471be838d18b408c379c
Guilherme Dall'Agnol Deconto <deconto100@gmail.com>
2021-08-25 20:12:55 -0300

    Reading resource file

--- src/io.cpp
+++ src/io.cpp
@@ -9,7 +9,7 @@ string io::getStringFromFile()
     ifstream inFile;
 
     //Opening the input file
-    inFile.open("files/20201-teste1.txt");
+    inFile.open("resources/20201-teste1.txt");
 
     stringstream strStream;
     //Reading file
